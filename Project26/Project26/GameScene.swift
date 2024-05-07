3ebc4d1b9dcfab356f9d1d6bcfc51c5f7fa68125
Vanya <whitewolf3310@icloud.com>
2024-05-07 14:26:47 +0300

    add project

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Ваня Науменко on 7.05.24.
+//
+
+import SpriteKit
+ 
+class GameScene: SKScene {
+    override func
+    didMove(to view: SKView) {
+        
+    }
+}
